52cad483187dd25e55777a6a18c56c97175d3b0c
kuangxianfeng <747779672@qq.com>
2024-01-11 17:17:21 +0800

    fix: 更改导出方式

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,4 @@ import * as object from './object'
 import * as tree from './tree'
 import * as reg from './reg'
 
-export default {
-    storage,
-    object,
-    tree,
-    reg
-}
+export { storage, object, tree, reg }
