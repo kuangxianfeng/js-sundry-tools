797526d6475aab4c9279de3773484f477a7c4db0
kuangxianfeng <747779672@qq.com>
2024-01-12 10:06:24 +0800

    fix: 提交

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,10 +2,4 @@ import * as storage from './storage';
 import * as object from './object';
 import * as tree from './tree';
 import * as reg from './reg';
-declare const _default: {
-    storage: typeof storage;
-    object: typeof object;
-    tree: typeof tree;
-    reg: typeof reg;
-};
-export default _default;
+export { storage, object, tree, reg };
